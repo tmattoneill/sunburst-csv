21f188ea2d037c41a32d1c5bd86b6b1680861714
Matt O'Neill <tmattoneill@gmail.com>
2025-01-05 11:42:28 +0000

    working and renamed

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      // Set the title for the browser tab
+      args[0].title = 'Sunburst 1.0';
+      return args;
+    });
+  },
 })
