55a31d396302b0a3e396728eaab6b2bbab54ab97
Matt O'Neill <tmattoneill@gmail.com>
2025-01-23 19:42:06 +0000

    small change to eslint and builder script

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,21 +1,23 @@
+const isProd = process.env.NODE_ENV === 'prod';
+
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    sourceType: 'module',
-    ecmaVersion: 'latest'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'prod' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'prod' ? 'warn' : 'off'
-  }
+ root: true,
+ env: {
+   node: true,
+   browser: true,
+   es2021: true
+ },
+ extends: [
+   'plugin:vue/vue3-essential',
+   'eslint:recommended'
+ ],
+ parserOptions: {
+   parser: '@babel/eslint-parser',
+   sourceType: 'module',
+   ecmaVersion: 'latest'
+ },
+ rules: {
+   'no-console': isProd ? 'warn' : 'off',
+   'no-debugger': isProd ? 'warn' : 'off'
+ }
 }
